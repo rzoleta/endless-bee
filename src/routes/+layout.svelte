942ec6eda1c1d81cf6752ff317ed5c3eac9d418b
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2026-05-08 18:33:33 +0800

    fix: favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import './layout.css';
-  import favicon from '$lib/assets/favicon.svg';
+  import favicon from '$lib/assets/favicon.ico';
   import appleTouchIcon from '$lib/assets/apple-touch-icon.png';
   import { ModeWatcher } from 'mode-watcher';
 
