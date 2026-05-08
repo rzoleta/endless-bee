371578d70184282bae4ca0cd52fc0f95f541be55
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2026-05-08 18:32:45 +0800

    style(ui): update icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
-	import { ModeWatcher } from 'mode-watcher';
+  import './layout.css';
+  import favicon from '$lib/assets/favicon.svg';
+  import appleTouchIcon from '$lib/assets/apple-touch-icon.png';
+  import { ModeWatcher } from 'mode-watcher';
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <ModeWatcher />
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+  <link rel="icon" href={favicon} />
+  <link rel="apple-touch-icon" href={appleTouchIcon} />
+</svelte:head>
 {@render children()}
